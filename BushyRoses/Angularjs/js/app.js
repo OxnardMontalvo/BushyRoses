f4d26ffcc3c59cc13204c6cc6417b5696eb630fc
Markus <markus_andersson8@yahoo.se>
2017-06-19 15:52:40 +0200

    Created customer page for notice of intrest. Added form and information.

--- BushyRoses/Angularjs/js/app.js
+++ BushyRoses/Angularjs/js/app.js
@@ -1,1 +1,20 @@
-﻿
+﻿/// <reference path="C:\Users\deltagare\Source\Repos\BushyRoses\BushyRoses\Scripts/angular.js" />
+/// <reference path="C:\Users\deltagare\Source\Repos\BushyRoses\BushyRoses\Scripts/angular-route.js" />
+
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "/Angularjs/html/mainPage.html",
+        controller: "collectForm",
+        controllerAs: "vm"
+    })
+    .when("/Home/Index.html", {
+        templateUrl: "/Angularjs/html/mainPage.html",
+        controller: "collectForm",
+        controllerAs: "vm"
+    })
+
+    .otherwise({ redirectTo: "/" });
+});
---
+++ BushyRoses/Angularjs/js/controllers.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="C:\Users\deltagare\Source\Repos\BushyRoses\BushyRoses\Scripts/angular.js" />
+/// <reference path="C:\Users\deltagare\Source\Repos\BushyRoses\BushyRoses\Scripts/angular-route.min.js" />
+
+angular.module("app")
+
+.controller("collectForm", function () {
+    var vm = this;
+
+    
+    if (vm.users == null) {
+        vm.users = [];
+    }
+
+    vm.user = {
+        Namn: "",
+        Adress: "",
+        Ort: "",
+        Postnr: "",
+        Epost: ""
+    };
+
+    vm.readForm = function () {
+        vm.users.push(vm.user);
+        console.log(vm.users);
+    }
+});
+
