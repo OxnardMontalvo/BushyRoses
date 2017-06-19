7253494540d50e26610062ad9878bdcaad683496
a <a@a.a>
2017-06-19 09:06:15 +0200

    Add project files.

---
+++ BushyRoses/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BushyRoses.Startup))]
+namespace BushyRoses
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
